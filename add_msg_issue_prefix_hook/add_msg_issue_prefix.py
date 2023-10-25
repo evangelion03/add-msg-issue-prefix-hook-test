079bbffce428341f6441df0d2e14a2cca347d587
EC2 Default User <ec2-user@ip-172-31-41-240.ap-northeast-1.compute.internal>
2023-10-25 09:01:29 +0000

    test export 3

--- add_msg_issue_prefix_hook/add_msg_issue_prefix.py
+++ add_msg_issue_prefix_hook/add_msg_issue_prefix.py
@@ -125,10 +125,10 @@ def main():
         if issue_number and not tag_present:
             prefix = template.format(issue_number)
             new_msg = modify_commit_message(content, prefix, insert_after)
-            f.write(default)
+            f.write(str(default))
         elif default and not tag_present:
             new_msg = modify_commit_message(content, default, insert_after)
-            f.write(default)
+            f.write(str(default))
         else:
             f.write(content)
 
